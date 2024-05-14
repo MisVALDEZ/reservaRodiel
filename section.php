f9ad3c1652d644f9a8532bc2be341dae9014bbfd
jeffry suero <jeffrysueroc@gmail.com>
2024-05-14 15:19:38 -0400

    update

---
+++ section.php
@@ -0,0 +1,83 @@
+<section class="banner_area">
+    <div class="booking_table d_flex align-items-center">
+        <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
+        <div class="container">
+            <div class="banner_content text-center">
+                <h6>conductores Capasitados</h6>
+                <h2>Viaje Seguro</h2>
+                <p>Conductores Capacitados para un Viaje Seguro y Confiable.
+                    Disfrute de Confort, Elegancia y Seguridad en cada trayecto.
+                    Reserve con Nosotros para una Experiencia de Transporte Inigualable.</p>
+                <!-- <a href="#" class="btn theme_btn button_hover">Get Started</a> -->
+            </div>
+        </div>
+    </div>
+    <div class="hotel_booking_area position">
+        <div class="container">
+            <div class="hotel_booking_table">
+                <div class="col-md-3">
+                    <h2>Reserva<br>Con Nosotro</h2>
+                </div>
+                <div class="col-md-9">
+                    <div class="boking_table">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="book_tabel_item">
+                                    <div class="form-group">
+                                        <div class='input-group date' id='datetimepicker11'>
+                                            <input type='text' class="form-control" placeholder="Arrival Date" />
+                                            <span class="input-group-addon">
+                                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class='input-group date' id='datetimepicker1'>
+                                            <input type='text' class="form-control" placeholder="Departure Date" />
+                                            <span class="input-group-addon">
+                                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="book_tabel_item">
+                                    <div class="input-group">
+                                        <select class="wide">
+                                            <option data-display="Adult">Adult</option>
+                                            <option value="1">Old</option>
+                                            <option value="2">Younger</option>
+                                            <option value="3">Potato</option>
+                                        </select>
+                                    </div>
+                                    <div class="input-group">
+                                        <select class="wide">
+                                            <option data-display="Child">Child</option>
+                                            <option value="1">Child</option>
+                                            <option value="2">Baby</option>
+                                            <option value="3">Child</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="book_tabel_item">
+                                    <div class="input-group">
+                                        <select class="wide">
+                                            <option data-display="Child">Number of Rooms</option>
+                                            <option value="1">Room 01</option>
+                                            <option value="2">Room 02</option>
+                                            <option value="3">Room 03</option>
+                                        </select>
+                                    </div>
+                                    <a class="btn btn-danger" href="#">Book Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
